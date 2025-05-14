c83448403c7c0ba8f2250b0c91b9f7b163a7e5a9
abhijeet4022 <abhijeet4022@gmail.com>
2025-05-14 07:38:21 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  ami           = data.aws_ami.devops-practice.id
   instance_type = "c6a.2xlarge"
   security_groups = [aws_security_group.sg.name]
 
