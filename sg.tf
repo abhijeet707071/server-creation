c7de8cf691560330e325837e56dce2b6a7be5b9d
abhijeet4022 <abhijeet4022@gmail.com>
2025-05-14 09:13:44 +0530

    Shell Scripting - Component Setup

--- sg.tf
+++ sg.tf
@@ -12,6 +12,7 @@ resource "aws_security_group" "sg" {
 # Inbound Rule: Allow all TCP traffic from anywhere
 resource "aws_vpc_security_group_ingress_rule" "allow_all_inbound" {
   security_group_id = aws_security_group.sg.id
+  description       = "Allow-all-inbound-traffic"
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 0
   to_port           = 65535
@@ -21,6 +22,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_all_inbound" {
 # Outbound Rule: Allow all protocols to anywhere
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.sg.id
+  description       = "Allow-all-outbound-traffic"
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
