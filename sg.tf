8be7db39a40965865a801acf72af59ca8bef65d3
abhijeet4022 <abhijeet4022@gmail.com>
2025-05-14 09:11:32 +0530

    Shell Scripting - Component Setup

--- sg.tf
+++ sg.tf
@@ -14,7 +14,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_all_inbound" {
   security_group_id = aws_security_group.sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 0
-  to_port           = 0
+  to_port           = 65535
   ip_protocol       = "tcp"
 }
 
